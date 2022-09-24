a758ec9b72681cddc8d67e7262d25bf0b8b4510e
balakrishnajonnadula <95881327+balakrishnajonnadula@users.noreply.github.com>
2022-09-24 12:15:11 +0530

    updated

--- src/myPackage/Tables.java
+++ src/myPackage/Tables.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Tables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		//Activated the Scanner
 		Scanner sc = new Scanner(System.in);
